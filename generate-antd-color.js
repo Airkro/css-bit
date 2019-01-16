664b5496bed8b92baecf1c4f6103f298d4c01f21
Eric Chen <airkro@qq.com>
2019-01-17 00:58:18 +0800

    refactor

--- generate-antd-color.js
+++ generate-antd-color.js
@@ -1,22 +1,23 @@
-const { generate, presetPalettes } = require("ant-design-palettes");
-const { mapValues, reduce, pickBy } = require("lodash");
-const os = require("os");
-const fs = require("fs-extra");
-const { resolve } = require("path");
+const {
+  generate,
+  presetPalettes: { grey, ...presetPalettes }
+} = require('@ant-design/colors');
+const { mapValues, reduce } = require('lodash');
+const { EOL } = require('os');
+const { outputFileSync } = require('fs-extra');
+const { resolve } = require('path');
 
-const neutral = generate("#bfbfbf");
-const preset = pickBy(presetPalettes, (_, key) => key !== "grey");
+const neutral = generate('#bfbfbf');
+const preset = { neutral, ...presetPalettes };
 
 function statement(colorName) {
-  return (value, index) => "$" + colorName + "-" + index + ": " + value + ";";
+  return (value, index) => `$${colorName}-${index}: ${value};`;
 }
 
 const data = reduce(
-  mapValues({ neutral, ...preset }, (colors, colorName) =>
-    colors.map(statement(colorName))
-  ),
-  (io, sets, name) => [...io, "//--" + name + "--------", ...sets, ""],
-  ["/* stylelint-disable color-hex-length */" + os.EOL]
-).join(os.EOL);
+  mapValues(preset, (colors, colorName) => colors.map(statement(colorName))),
+  (io, sets, name) => [...io, `//--${name}--------`, ...sets, ''],
+  ['/* stylelint-disable color-hex-length */', EOL]
+).join(EOL);
 
-fs.outputFileSync(resolve("./package/antd-color.scss"), data);
+outputFileSync(resolve('./package/antd-color.scss'), data);
